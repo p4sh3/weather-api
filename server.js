240a2d578fb941b063f861eed68bcdae4cbef04f
Carlos Alberto Pacheco Sánchez <pachecco415@gmail.com>
2024-07-10 02:45:14 -0600

    Delete frontend

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
-import path from "node:path"
 import { coordinatesRouter } from "./routes/coordenadasRoutes.js";
 import { weatherRouter } from "./routes/climaRoutes.js";
 
@@ -10,19 +9,16 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://weather-map-dev.netlify.app/',
+  optionsSuccessStatus: 200,
+  methods: ["GET"]
+}))
 app.use(express.json());
-app.use(express.static(path.join(import.meta.dirname, "public", "dist")));
 
 
-
-
-app.get("/", (_, res) => {
-  res.sendFile(path.join(import.meta.dirname, "public", "dist", "index.html"));
-});
-
-app.use("/coordenadas", coordinatesRouter)
-app.use("/clima", weatherRouter)
+app.use("/api/coordenadas", coordinatesRouter)
+app.use("/api/clima", weatherRouter)
 
 // Middleware de manejo de errores
 app.use((err, req, res, next) => {
